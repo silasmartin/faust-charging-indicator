4e98188b88b611cb343e63a4fe42ae11f9049473
Silas Martin <martinsilas123@outlook.de>
2023-12-18 11:45:37 +0100

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  IsKeptAwakeResult,
+  IsSupportedResult,
+  KeepAwake,
+} from '@capacitor-community/keep-awake';
+import { BatteryInfo, Device } from '@capacitor/device';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  batteryInfo?: BatteryInfo;
+  isKeptAwake?: IsKeptAwakeResult;
+  keepAwakeSupported?: IsSupportedResult;
+
+  testnumber = 0;
+
+  interval: any;
 
   constructor() {}
 
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      this.checkInfo();
+      this.testnumber += 1;
+    }, 5000);
+  }
+
+  checkInfo() {
+    Device.getBatteryInfo().then((info) => (this.batteryInfo = info));
+    KeepAwake.isSupported().then((x) => (this.keepAwakeSupported = x));
+    KeepAwake.isKeptAwake().then((x) => (this.isKeptAwake = x));
+    console.log(this.batteryInfo, this.isKeptAwake, this.keepAwakeSupported);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
